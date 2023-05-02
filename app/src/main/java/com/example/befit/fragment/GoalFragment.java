224ff1580041437857bc2150d2bf105b4831884b
Jz5zp <1056166100@qq.com>
2023-05-02 12:31:23 +0800

    initial commit

---
+++ app/src/main/java/com/example/befit/fragment/GoalFragment.java
@@ -0,0 +1,28 @@
+package com.example.befit.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.befit.databinding.GoalFragmentBinding;
+
+public class GoalFragment extends Fragment {
+    private GoalFragmentBinding addBinding;
+    public GoalFragment(){}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the View for this fragment
+        addBinding = GoalFragmentBinding.inflate(inflater, container, false);
+        View view = addBinding.getRoot();
+        return view;
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        addBinding = null;
+    }
+}
---
+++ app/src/main/java/com/example/befit/fragment/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.example.befit.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.befit.databinding.HomeFragmentBinding;
+
+public class HomeFragment extends Fragment {
+    private HomeFragmentBinding addBinding;
+    public HomeFragment(){}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the View for this fragment
+        addBinding = HomeFragmentBinding.inflate(inflater, container, false);
+        View view = addBinding.getRoot();
+        return view;
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        addBinding = null;
+    }
+}
