c2be1222e589f2b4054402cc88b5f3026bb3b559
Jz5zp <1056166100@qq.com>
2023-05-02 12:29:42 +0800

    initial commit

--- app/src/main/java/com/example/befit/MainActivity.java
+++ app/src/main/java/com/example/befit/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.befit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.befit.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
+    private AppBarConfiguration mAppBarConfiguration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        setSupportActionBar(binding.appBar.toolbar);
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home_fragment,
+                R.id.nav_club_fragment,
+                R.id.nav_book_classes_fragment,
+                R.id.nav_my_classes_fragment,
+                R.id.nav_goal_fragment,
+                R.id.nav_profile_fragment)
+                //to display the Navigation button as a drawer symbol,not being shown as an Up button
+                .setOpenableLayout(binding.drawerLayout)
+                .build();
+        FragmentManager fragmentManager= getSupportFragmentManager();
+        NavHostFragment navHostFragment = (NavHostFragment)
+                fragmentManager.findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        //Sets up a NavigationView for use with a NavController.
+        NavigationUI.setupWithNavController(binding.navView, navController);
+        //Sets up a Toolbar for use with a NavController.
+        NavigationUI.setupWithNavController(binding.appBar.toolbar,navController,
+                mAppBarConfiguration);
     }
 }
