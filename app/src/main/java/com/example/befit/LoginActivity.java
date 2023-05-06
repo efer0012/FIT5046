1c70c7a02ae177d05a5f39c4756821e3a9cb836c
WenxuanZ-Monash <110577439+WenxuanZ-Monash@users.noreply.github.com>
2023-05-06 23:57:35 +1000

    Add login screen

---
+++ app/src/main/java/com/example/befit/LoginActivity.java
@@ -0,0 +1,57 @@
+package com.example.befit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        //FirebaseApp.initializeApp(this);
+        auth = FirebaseAuth.getInstance();
+        EditText emailEditText = findViewById(R.id.emailEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+        Button loginButton = findViewById(R.id.submitButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_Email = emailEditText.getText().toString();
+                String txt_Pwd = passwordEditText.getText().toString();
+                loginUser(txt_Email, txt_Pwd);
+            }
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void loginUser(String txt_email, String txt_pwd) {
+        // call the object and provide it with email id and password
+        auth.signInWithEmailAndPassword(txt_email, txt_pwd).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                toastMsg("Login Successful");
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+    }
+    public void toastMsg(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+
+}
